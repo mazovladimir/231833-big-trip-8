674126cc4063efdc1ce85fdb29a18db530f06f77
Vladimir Mazo <Vladimir.Mazo@icl-services.com>
2019-02-27 19:25:24 +0300

    Added trips

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import makeFilter from './make-filter.js';
+import makeTask from './make-task.js';
 
-const filters = document.querySelector(`.trip-filter`);
 
-filters.insertAdjacentHTML(`beforeend`, makeFilter(`Everything`, true));
-filters.insertAdjacentHTML(`beforeend`, makeFilter(`Future`));
-filters.insertAdjacentHTML(`beforeend`, makeFilter(`Past`));
+const tripFilter = document.querySelector(`.trip-filter`);
+
+
+tripFilter.insertAdjacentHTML(`beforeend`, makeFilter(`Everything`, true));
+tripFilter.insertAdjacentHTML(`beforeend`, makeFilter(`Future`));
+tripFilter.insertAdjacentHTML(`beforeend`, makeFilter(`Past`));
+
+const tripDayItems = document.querySelector(`.trip-day__items`);
+
+tripDayItems.insertAdjacentHTML(`beforeend`, makeTask('icon', 'Taxi to Airport', '10:00', '11:00', '1h 30s', '20', ['order1', 'order2']));
+
---
+++ src/make-task.js
@@ -0,0 +1,23 @@
+export default (icon, title, starttime, endtime, duration, price, offers) => 
+`
+    <article class="trip-point">
+        <i class="trip-icon">${icon}</i>
+        <h3 class="trip-point__title">${title}</h3>
+        <p class="trip-point__schedule">
+            <span class="trip-point__timetable">${starttime}&nbsp;&mdash; ${endtime}</span>
+            <span class="trip-point__duration">${duration}</span>
+        </p>
+        <p class="trip-point__price">&euro;&nbsp;${price}</p>
+        ${createOffers(offers)}
+    </article>
+`
+
+let createOffers = (offers) =>
+        `<ul class="trip-point__offers">
+            ${offers.map(offer =>`
+                <li>
+                    <button class="trip-point__offer">
+                        ${offer}
+                    </button>
+                 </li>`).join(``)}
+        </ul>`
