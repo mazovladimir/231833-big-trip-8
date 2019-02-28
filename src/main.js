c74d3f9fc659ce857a80ce29372d81052526c380
Vladimir Mazo <Vladimir.Mazo@icl-services.com>
2019-02-28 13:12:23 +0300

    eslint passed

--- src/main.js
+++ src/main.js
@@ -10,35 +10,35 @@ tripFilter.insertAdjacentHTML(`beforeend`, makeFilter(`Future`));
 tripFilter.insertAdjacentHTML(`beforeend`, makeFilter(`Past`));
 
 const renderTrips = (tag) => {
-    const randomTask = getRandomTask(5, 1);
-    for (let i = 0; i < randomTask; i++) {
-        tag.insertAdjacentHTML(`beforeend`, makeTask('icon', 'Taxi to Airport', '10:00', '11:00', '1h 30s', '20', ['order1', 'order2']));
-    }
-}
+  const randomTask = getRandomTask(5, 1);
+  for (let i = 0; i < randomTask; i++) {
+    tag.insertAdjacentHTML(`beforeend`, makeTask(`icon`, `Taxi to Airport`, `10:00`, `11:00`, `1h 30s`, `20`, [`order1`, `order2`]));
+  }
+};
 
 renderTrips(tripDayItems);
 
-const filterEverything = document.querySelector("#filter-everything");
-const filterFuture = document.querySelector("#filter-future");
-const filterPast = document.querySelector("#filter-past");
+const filterEverything = document.querySelector(`#filter-everything`);
+const filterFuture = document.querySelector(`#filter-future`);
+const filterPast = document.querySelector(`#filter-past`);
 
 
 const changeTrips = () => {
-    const tripPoints = document.querySelectorAll(".trip-point");
-    tripPoints.forEach(function(trip) {
-        trip.remove();
-    });
-    renderTrips(tripDayItems);
-}
+  const tripPoints = document.querySelectorAll(`.trip-point`);
+  tripPoints.forEach(function (trip) {
+    trip.remove();
+  });
+  renderTrips(tripDayItems);
+};
 
-filterEverything.addEventListener('click', () => {
-    changeTrips();
+filterEverything.addEventListener(`click`, () => {
+  changeTrips();
 });
 
-filterFuture.addEventListener('click', () => {
-    changeTrips();
+filterFuture.addEventListener(`click`, () => {
+  changeTrips();
 });
 
-filterPast.addEventListener('click', () => {
-    changeTrips();
+filterPast.addEventListener(`click`, () => {
+  changeTrips();
 });
--- src/random-trip.js
+++ src/random-trip.js
@@ -1,6 +1,6 @@
-export default function(max, min) {
-    if (typeof min === 'undefined') {
-      min = 0;
-    }
-    return Math.floor(Math.random() * (max - min + 1)) + min
+export default function (max, min) {
+  if (typeof min === `undefined`) {
+    min = 0;
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
